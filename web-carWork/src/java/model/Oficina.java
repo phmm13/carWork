069a7a95565b137f142cea7abcf9115a308819cd
Pedro Henrique Marcelino Matias - 010102670 <pedroh@sescdf.com.br>
2018-10-24 14:27:56 -0300

    Correção das classes de acordo com o banco

--- web-carWork/src/java/model/Oficina.java
+++ web-carWork/src/java/model/Oficina.java
@@ -16,6 +16,14 @@ public class Oficina {
     private int id_oficina;
     private String nome_oficina;
     private String telefone_oficina;
+    private String cnpj_oficina;
+    private String eml_oficina;
+    private String usr_oficina;
+    private String pwd_oficina;
+    private String cep_oficina;
+    private String lgd_oficina;
+    private String bai_oficina;
+    private int num_oficina;
     private TipoOficina tipoOfcina;
 
     public int getId_oficina() {
@@ -42,6 +50,70 @@ public class Oficina {
         this.telefone_oficina = telefone_oficina;
     }
 
+    public String getCnpj_oficina() {
+        return cnpj_oficina;
+    }
+
+    public void setCnpj_oficina(String cnpj_oficina) {
+        this.cnpj_oficina = cnpj_oficina;
+    }
+
+    public String getEml_oficina() {
+        return eml_oficina;
+    }
+
+    public void setEml_oficina(String eml_oficina) {
+        this.eml_oficina = eml_oficina;
+    }
+
+    public String getUsr_oficina() {
+        return usr_oficina;
+    }
+
+    public void setUsr_oficina(String usr_oficina) {
+        this.usr_oficina = usr_oficina;
+    }
+
+    public String getPwd_oficina() {
+        return pwd_oficina;
+    }
+
+    public void setPwd_oficina(String pwd_oficina) {
+        this.pwd_oficina = pwd_oficina;
+    }
+
+    public String getCep_oficina() {
+        return cep_oficina;
+    }
+
+    public void setCep_oficina(String cep_oficina) {
+        this.cep_oficina = cep_oficina;
+    }
+
+    public String getLgd_oficina() {
+        return lgd_oficina;
+    }
+
+    public void setLgd_oficina(String lgd_oficina) {
+        this.lgd_oficina = lgd_oficina;
+    }
+
+    public String getBai_oficina() {
+        return bai_oficina;
+    }
+
+    public void setBai_oficina(String bai_oficina) {
+        this.bai_oficina = bai_oficina;
+    }
+
+    public int getNum_oficina() {
+        return num_oficina;
+    }
+
+    public void setNum_oficina(int num_oficina) {
+        this.num_oficina = num_oficina;
+    }
+    
     public TipoOficina getTipoOfcina() {
         return tipoOfcina;
     }
--- web-carWork/src/java/model/OficinaDAO.java
+++ web-carWork/src/java/model/OficinaDAO.java
@@ -16,24 +16,44 @@ import java.util.ArrayList;
  */
 public class OficinaDAO extends DataBaseDAO{
     public void inserir(Oficina of) throws Exception {
-        String sql = "INSERT INTO tb_oficina (nme_oficina,tel_oficina,cod_tipo_oficina) VALUES (?,?,?)";
+        String sql = "INSERT INTO tb_oficina (nme_oficina,tel_oficina,cnpj_oficina,eml_oficina,usr_oficina,pwd_oficina,cep_oficina,lgd_oficina,bai_oficina,num_oficina,cod_tipo_oficina)"
+                + " VALUES (?,?,?,?,?,?,?,?,?,?,?)";
         this.conectar();
         PreparedStatement pstm = cn.prepareStatement(sql);
         pstm.setString(1, of.getNome_oficina());
         pstm.setString(2, of.getTelefone_oficina());
-        pstm.setInt(3, of.getTipoOfcina().getId_tipo_oficina());
+        pstm.setString(3,of.getCnpj_oficina());
+        pstm.setString(4,of.getEml_oficina());
+        pstm.setString(5,of.getUsr_oficina());
+        pstm.setString(6,of.getPwd_oficina());
+        pstm.setString(7, of.getCep_oficina());
+        pstm.setString(8,of.getLgd_oficina());
+        pstm.setString(9,of.getBai_oficina());
+        pstm.setInt(10, of.getNum_oficina());
+        pstm.setInt(11, of.getTipoOfcina().getId_tipo_oficina());
         pstm.execute();
         this.desconectar();
     }
 
     public void alterar(Oficina of) throws Exception {
-        String sql = "UPDATE tb_oficina SET nme_oficina=?,tel_oficina=?,cod_tipo_oficina=? WHERE idt_oficina=?";
+        //nme_oficina,tel_oficina,cnpj_oficina,eml_oficina,usr_oficina,pwd_oficina,cep_oficina,lgd_oficina,bai_oficina,num_oficina,cod_tipo_oficina
+        String sql = "UPDATE tb_oficina SET nme_oficina=?,tel_oficina=?,cnpj_oficina=?"
+                + ",eml_oficina=?,usr_oficina=?,pwd_oficina=?,cep_oficina=?,lgd_oficina=?"
+                + ",bai_oficina=?,num_oficina=?,cod_tipo_oficina=? "
+                + "WHERE idt_oficina=?";
         this.conectar();
         PreparedStatement pstm = cn.prepareStatement(sql);
         pstm.setString(1, of.getNome_oficina());
         pstm.setString(2, of.getTelefone_oficina());
-        pstm.setInt(3, of.getTipoOfcina().getId_tipo_oficina());
-        pstm.setInt(4, of.getId_oficina());
+        pstm.setString(3,of.getCnpj_oficina());
+        pstm.setString(4,of.getEml_oficina());
+        pstm.setString(5,of.getUsr_oficina());
+        pstm.setString(6,of.getPwd_oficina());
+        pstm.setString(7, of.getCep_oficina());
+        pstm.setString(8,of.getLgd_oficina());
+        pstm.setString(9,of.getBai_oficina());
+        pstm.setInt(10, of.getNum_oficina());
+        pstm.setInt(11, of.getTipoOfcina().getId_tipo_oficina());
         pstm.execute();
         this.desconectar();
     }
@@ -58,6 +78,14 @@ public class OficinaDAO extends DataBaseDAO{
             of.setId_oficina(rs.getInt("idt_oficina"));
             of.setNome_oficina(rs.getString("nme_oficina"));
             of.setTelefone_oficina(rs.getString("tel_oficina"));
+            of.setCnpj_oficina(rs.getString("cnpj_oficina"));
+            of.setEml_oficina(rs.getString("eml_oficina"));
+            of.setUsr_oficina(rs.getString("usr_oficina"));
+            of.setPwd_oficina(rs.getString("pwd_oficina"));
+            of.setCep_oficina(rs.getString("cep_oficina"));
+            of.setLgd_oficina(rs.getString("lgd_oficina"));
+            of.setBai_oficina(rs.getString("bai_oficina"));
+            of.setNum_oficina(rs.getInt("num_oficina"));
             
             TipoOficina to = new TipoOficina();
             to.setId_tipo_oficina(rs.getInt("cod_tipo_oficina"));
@@ -81,6 +109,14 @@ public class OficinaDAO extends DataBaseDAO{
             of1.setId_oficina(rs.getInt("idt_oficina"));
             of1.setNome_oficina(rs.getString("nme_oficina"));
             of1.setTelefone_oficina(rs.getString("tel_oficina"));
+            of1.setCnpj_oficina(rs.getString("cnpj_oficina"));
+            of1.setEml_oficina(rs.getString("eml_oficina"));
+            of1.setUsr_oficina(rs.getString("usr_oficina"));
+            of1.setPwd_oficina(rs.getString("pwd_oficina"));
+            of1.setCep_oficina(rs.getString("cep_oficina"));
+            of1.setLgd_oficina(rs.getString("lgd_oficina"));
+            of1.setBai_oficina(rs.getString("bai_oficina"));
+            of1.setNum_oficina(rs.getInt("num_oficina"));
             
             TipoOficina to = new TipoOficina();
             to.setId_tipo_oficina(rs.getInt("cod_tipo_oficina"));
