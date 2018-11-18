111ebb77ae1f5514dc397616b6433017400abc26
unknown <pedromarcelino699@gmail.com>
2018-11-18 15:38:40 -0200

    Alteração driver jdbc

--- webCarWork/src/java/model/DataBaseDAO.java
+++ webCarWork/src/java/model/DataBaseDAO.java
@@ -8,6 +8,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * @author Pedro Henrique Marcelino Matias
@@ -16,12 +17,28 @@ import java.sql.DriverManager;
 public class DataBaseDAO {
     Connection cn;
     public void conectar() throws Exception{
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         //alterar a senha conforme a maquina virtual
+        
+        /*
+        
+        String banco = "carwork";
+        String user = "root";
+        String password = "carwork";
+        String endereco = "142.93.192.165/";
+        
+        */
+        
         String banco = "carwork";
         String user = "root";
         String password = "";
-        cn = DriverManager.getConnection("jdbc:mysql://localhost/"+banco,user,password);
+        String endereco = "localhost/";
+        
+        /*
+        
+        */
+        cn = DriverManager.getConnection("jdbc:mysql://"+endereco+banco+"?autoReconnect=true&useSSL=false&useTimezone=true&serverTimezone=UTC",user,password);
+        
     }
     
     public void desconectar() throws Exception{
