e40cc4c0ed544ae6012f05605de210205d9a251a
Pedro Henrique <pedromarcelino699@gmail.com>
2018-11-25 13:46:35 -0200

    Correção databaseDAO

--- webCarWork/src/java/model/DataBaseDAO.java
+++ webCarWork/src/java/model/DataBaseDAO.java
@@ -20,23 +20,15 @@ public class DataBaseDAO {
         Class.forName("com.mysql.cj.jdbc.Driver");
         //alterar a senha conforme a maquina virtual
         
-        /*
+        
         
         String banco = "carwork";
         String user = "root";
-        String password = "carwork";
-        String endereco = "142.93.192.165/";
+        String password = "carwork"; //""
+        String endereco = "173.82.173.74/"; //localhost
         
-        */
         
-        String banco = "carwork";
-        String user = "root";
-        String password = "";
-        String endereco = "localhost/";
         
-        /*
-        
-        */
         cn = DriverManager.getConnection("jdbc:mysql://"+endereco+banco+"?autoReconnect=true&useSSL=false&useTimezone=true&serverTimezone=UTC",user,password);
         
     }
